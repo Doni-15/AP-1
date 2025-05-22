b0467c33dff35f95671444e1b86e23ee4e4e429e
Doni <donisimamora15@gmail.com>
2025-05-22 21:34:25 +0700

    Update belajar_fungsi.cpp

--- Pertemuan 6/belajar_fungsi.cpp
+++ Pertemuan 6/belajar_fungsi.cpp
@@ -1,13 +1,18 @@
-#include <iostream>
-using namespace std;
+#include <iostream> 
+using namespace std; 
 
+// Fungsi sapa: menerima parameter berupa string (nama)
 void sapa(string nama){
+    // Menampilkan sapaan dengan nama yang diberikan sebagai parameter
     cout << "Hallo " << nama << "! Selamat belajar C++" << endl;
 }
 
 int main(){
+    // Mendeklarasikan dan menginisialisasi variabel nama_pengguna
     string nama_pengguna = "Doni";
-    sapa(nama_pengguna);
 
-    return 0;
+    // Memanggil fungsi sapa dan mengirimkan nama_pengguna sebagai argumen
+    sapa(nama_pengguna);  // Maka output-nya adalah: "Hallo Doni! Selamat belajar C++"
+
+    return 0; 
 }
