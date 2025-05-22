594ec929845cb51a32456a33690b523c5c14f172
Doni <donisimamora15@gmail.com>
2025-05-22 21:36:10 +0700

    Update header_custom.h

--- Pertemuan 6/Belajar Header/header_custom.h
+++ Pertemuan 6/Belajar Header/header_custom.h
@@ -1,8 +1,8 @@
-#define u using namespace std;
-#define L <<
-#define i #include <iostream>
-#define c #include <cmath>
-#define h #include "header_custom.h"
-#define int main(){ begin
-#define } end
-#define s system("cls")
+#define u using namespace std;        // Mendefinisikan 'u' sebagai 'using namespace std;'
+#define L <<                         // Mendefinisikan 'L' sebagai operator << untuk output
+#define i #include <iostream>        // Mendefinisikan 'i' sebagai include iostream
+#define c #include <cmath>           // Mendefinisikan 'c' sebagai include cmath (untuk fungsi matematika)
+#define h #include "header_custom.h" // Mendefinisikan 'h' sebagai include header buatan sendiri
+#define int main(){ begin            // Mendefinisikan int main(){ sebagai begin (nanti diganti begin di bawah)
+#define } end                        // Mendefinisikan } sebagai end (nanti diganti end di bawah)
+#define s system("cls")              // Mendefinisikan 's' sebagai perintah untuk membersihkan layar konsol (khusus Windows)
