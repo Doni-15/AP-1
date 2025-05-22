6017059e3514f803d1236df468d9710d284cf6f7
Doni <donisimamora15@gmail.com>
2025-05-22 20:35:48 +0700

    Update latihan2.cpp

--- Pertemuan 4/latihan2.cpp
+++ Pertemuan 4/latihan2.cpp
@@ -2,15 +2,28 @@
 using namespace std;
 
 int main() {
-    system("cls");
+    system("cls"); 
 
-    string kalimat;
-    cout << "Masukkan kalimat: "; getline(cin, kalimat);
+    string kalimat; // Variabel untuk menyimpan input kalimat
+    cout << "Masukkan kalimat: "; 
+    getline(cin, kalimat); // Menggunakan getline agar input bisa berupa kalimat lengkap (termasuk spasi).
 
-    for (int i = 0; i < kalimat.length(); i++) {
-        if (kalimat[i] == 'a' || kalimat[i] == 'e' || kalimat[i] == 'i' || kalimat[i] == 'o' || kalimat[i] == 'u'){
+    for (int i = 0; i < kalimat.length(); i++) { // Melakukanperulangan dari indeks 0 sampai panjang kalimat - 1. kalimat.length() digunakan untuk mengetahui jumlah karakter dalam string.
+        /*
+            Mengecek apakah karakter ke-i merupakan huruf vokal kecil.
+            Jika iya, maka karakter tersebut akan diubah menjadi huruf kapital.
+         */
+        if (kalimat[i] == 'a' || kalimat[i] == 'e' || 
+            kalimat[i] == 'i' || kalimat[i] == 'o' || 
+            kalimat[i] == 'u') {
+
             kalimat[i] = toupper(kalimat[i]);
+            /*
+               Fungsi toupper() mengubah huruf kecil menjadi huruf besar.
+               Contoh: 'a' → 'A', 'e' → 'E', dst.
+            */
         }
     }
-    cout << "Kalimat hasil konversi: " << kalimat << endl;
+
+    cout << "Kalimat hasil konversi: " << kalimat << endl; // Menampilkan hasil akhir kalimat setelah huruf vokalnya diubah menjadi kapital.
 }
