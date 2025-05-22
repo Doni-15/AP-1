f45b9dd78f2a008d0bd04a1ad37e20fe0f0e1b2d
Doni <donisimamora15@gmail.com>
2025-05-22 20:17:30 +0700

    Update latihan.cpp

--- Pertemuan 2/latihan.cpp
+++ Pertemuan 2/latihan.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    float jari;
-    cout << "Masukkan jari-jari = "; cin >> jari;
-    
-    float volume = 4 / float(3) * (3.14) * jari * jari * jari;
-    cout << "Luas lingkaran = " << volume << endl;
+int main() {
+    float jari; // Variabel untuk menyimpan nilai jari-jari bola
 
+    cout << "Masukkan jari-jari = "; 
+    cin >> jari; // Meminta input dari pengguna berupa jari-jari bola
+
+    /*
+        Menghitung volume bola
+                        V = (4/3) × π × r^3
+
+        Contoh:
+        Jika jari-jari = 2,
+        Maka volume = 4/3 * 3.14 * 2^3 = 4/3 * 3.14 * 8 ≈ 33.49
+    */
+    float volume = 4 / float(3) * (3.14) * jari * jari * jari;  // Pakai float(3) biar hasilnya di baca float atau ada koma nya
+
+    cout << "Volume bola = " << volume << endl; // Menampilkan hasil volume bola
+
+    /*
+        Menghitung luas permukaan bola.
+                        L = 4 × π × r^2
+
+        Contoh:
+        Jika jari-jari = 2,
+        Maka luas permukaan = 4 * 3.14 * 2^2 = 4 * 3.14 * 4 = 50.24
+    */
     float luas_permukaan = 4 * (3.14) * jari * jari;
-    cout << "LUas permukaan = " << luas_permukaan;
+
+    cout << "Luas permukaan = " << luas_permukaan; // Menampilkan hasil luas permukaan bola
 }
