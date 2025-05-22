a98c2191ab7d0f9743c886c63be7cd009723ea99
Doni <donisimamora15@gmail.com>
2025-05-22 20:30:45 +0700

    Update pretest.cpp

--- Pertemuan 3/pretest.cpp
+++ Pertemuan 3/pretest.cpp
@@ -3,9 +3,28 @@ using namespace std;
 
 int main(){
     int x = 3;
-    int y = ++x + x++;
-    cout << x << " " << y << endl;
 
+    /* 
+       Operasi: ++x + x++
+       1. ++x: pre-increment, jadi x dinaikkan dulu dari 3 menjadi 4, lalu nilainya dipakai → hasil = 4
+       2. x++: post-increment, jadi nilai x sekarang 4, dipakai dulu sebagai 4, lalu baru dinaikkan menjadi 5
+       Maka:
+         - ++x = 4  (x jadi 4)
+         - x++ = 4  (x tetap 4 saat dijumlahkan, tapi jadi 5 setelah operasi)
+       Jadi:
+         y = 4 + 4 = 8
+         x setelah semua proses = 5
+    */
+    int y = ++x + x++;
+    cout << x << " " << y << endl;  // Output: 5 8
+
+    /*
+       Bitwise Left Shift (<<):
+       48 << 2 artinya geser bit ke kiri sebanyak 2 kali
+       48 dalam biner: 0011 0000
+       Shift 2 bit ke kiri: 1100 0000 → ini adalah 192
+       Secara matematis: 48 * (2^2) = 48 * 4 = 192
+    */
     int z = 48 << 2;
-    cout << z << endl;
+    cout << z << endl; // Output: 192
 }
